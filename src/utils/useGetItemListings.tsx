717049d67a8daab83e985a934b4426526691e671
Izzati Rafi <izzatirafi@Izzatis-MacBook-Air.local>
2025-07-05 09:53:21 +0800

    for ozzie juice

--- src/utils/useGetItemListings.tsx
+++ src/utils/useGetItemListings.tsx
@@ -1,115 +1,115 @@
 export const useGetItemListings = () => {
   const data = [
     {
-      id: "twin-choco-hazelnut",
-      url: `/media/twin-chocolate-hazelnut.jpeg`,
+      id: "mango",
+      url: `/media/mango.png`,
       alt: "theozziecookies",
-      price: 10.90,
-      name: "Twin Choco Hazelnut",
+      price: 8.00,
+      name: "Iced or Smoothie Mango Float",
       description:
-        "Double the Chocolate, Double the Indulgence! Rich Dark Chocolate & Hazelnut Stuffed Perfection!",
+        "Iced or Smoothie Mango Float",
       stock: 1,
     },
     {
-      id: "red-velvet-cheese-cake",
-      url: `/media/red-velvet.jpeg`,
+      id: "pineapple",
+      url: `/media/pineapple.png`,
       alt: "theozziecookies",
-      price: 10.90,
-      name: "Red Velvet Cheese Cake",
+      price: 11.00,
+      name: "Smoothie Pina Colada",
       description:
-        "Indulge in Luxury—Red Velvet Cookie Stuffed with White Chocolate & Cream Cheese",
+        "Smoothie Pina Colada",
       stock: 1,
     },
-    {
-      id: "uji-matcha-zie",
-      url: `/media/matcha.jpeg`,
-      alt: "theozziecookies",
-      price: 9.80,
-      name: "Uji Matcha Zie",
-      description:
-        "Matcha Meets White Chocolate—A Perfect Harmony of Flavor!",
-      stock: 1,
-    },
-    {
-      id: "lotus-biscoff",
-      url: `/media/lotus-biscoff.jpeg`,
-      alt: "theozziecookies",
-      price: 9.80,
-      name: "Lotus Biscoff",
-      description:
-        "Stuffed with Lotus Biscoff & White Chocolate—A Cookie Dream Come True!",
-      stock: 1,
-    },
-    {
-      id: "macademia-white-chocolate",
-      url: `/media/macademia.jpeg`,
-      alt: "theozziecookies",
-      price: 10.90,
-      name: "Macademia White Chocolate",
-      description:
-        "Macademia White Chocolate",
-      stock: 1,
-    },
-    {
-      id: "monster-cookie",
-      url: `/media/monster.jpeg`,
-      alt: "theozziecookies",
-      price: 9.80,
-      name: "Monster Ozzie",
-      description:
-        "The Ultimate Cookie Mashup! Oreo × Chips Ahoy × White Chocolate x Milk Chocolate = Pure Bliss!",
-      stock: 0,
-    },
-    {
-      id: "signature-ozzie",
-      url: `/media/signature.jpeg`,
-      alt: "theozziecookies",
-      price: 8.80,
-      name: "Signature Ozzie / Original",
-      description:
-        "Soft, Chewy, and Loaded with Dark Chocolate Chips. A Timeless Classic in Every Bite, Just Like Grandma Used to Make!",
-      stock: 1,
-    },
-    {
-      id: "campfire-smores",
-      url: `/media/smores.jpeg`,
-      alt: "theozziecookies",
-      price: 9.80,
-      name: "Campfire Smores",
-      description:
-        "Classic Cookie with dark cholocate chip + Marshmallow on top",
-      stock: 1,
-    },
-    {
-      id: "mozie",
-      url: `/media/mochi.jpeg`,
-      alt: "theozziecookies",
-      price: 16.00,
-      name: "Ozzie MOCHI CHEESEE CAKE",
-      description:
-        "MOCHI CHEESE CAKE",
-      stock: 1,
-    },
-    {
-      id: "tiramisu",
-      url: `/media/tiramisu.jpeg`,
-      alt: "theozziecookies",
-      price: 14.00,
-      name: "Ozzie TIRAMISU",
-      description:
-        "TIRAMISU",
-      stock: 1,
-    },
-    {
-      id: "combo-best-seller",
-      url: `/media/combo-best-seller.jpg`,
-      alt: "theozziecookies",
-      price: 25.00,
-      name: "COMBO BEST SELLER",
-      description:
-        "COMBO BEST SELLER",
-      stock: 1,
-    }
+    // {
+    //   id: "uji-matcha-zie",
+    //   url: `/media/matcha.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 9.80,
+    //   name: "Uji Matcha Zie",
+    //   description:
+    //     "Matcha Meets White Chocolate—A Perfect Harmony of Flavor!",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "lotus-biscoff",
+    //   url: `/media/lotus-biscoff.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 9.80,
+    //   name: "Lotus Biscoff",
+    //   description:
+    //     "Stuffed with Lotus Biscoff & White Chocolate—A Cookie Dream Come True!",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "macademia-white-chocolate",
+    //   url: `/media/macademia.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 10.90,
+    //   name: "Macademia White Chocolate",
+    //   description:
+    //     "Macademia White Chocolate",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "monster-cookie",
+    //   url: `/media/monster.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 9.80,
+    //   name: "Monster Ozzie",
+    //   description:
+    //     "The Ultimate Cookie Mashup! Oreo × Chips Ahoy × White Chocolate x Milk Chocolate = Pure Bliss!",
+    //   stock: 0,
+    // },
+    // {
+    //   id: "signature-ozzie",
+    //   url: `/media/signature.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 8.80,
+    //   name: "Signature Ozzie / Original",
+    //   description:
+    //     "Soft, Chewy, and Loaded with Dark Chocolate Chips. A Timeless Classic in Every Bite, Just Like Grandma Used to Make!",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "campfire-smores",
+    //   url: `/media/smores.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 9.80,
+    //   name: "Campfire Smores",
+    //   description:
+    //     "Classic Cookie with dark cholocate chip + Marshmallow on top",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "mozie",
+    //   url: `/media/mochi.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 16.00,
+    //   name: "Ozzie MOCHI CHEESEE CAKE",
+    //   description:
+    //     "MOCHI CHEESE CAKE",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "tiramisu",
+    //   url: `/media/tiramisu.jpeg`,
+    //   alt: "theozziecookies",
+    //   price: 14.00,
+    //   name: "Ozzie TIRAMISU",
+    //   description:
+    //     "TIRAMISU",
+    //   stock: 1,
+    // },
+    // {
+    //   id: "combo-best-seller",
+    //   url: `/media/combo-best-seller.jpg`,
+    //   alt: "theozziecookies",
+    //   price: 25.00,
+    //   name: "COMBO BEST SELLER",
+    //   description:
+    //     "COMBO BEST SELLER",
+    //   stock: 1,
+    // }
   ];
 
   return {
